22c69f890a156f9a2f4d913bd899601d1a5bf4cd
saransaranya1997 <43058916+saransaranya1997@users.noreply.github.com>
2018-09-07 12:30:40 +0530

    Add files via upload

---
+++ floating.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float c,d,e,f,b;
+    a=6;
+    b=3;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+   System.out.println("The sum of two numbers:"+c);
+   System.out.println("The difference of two numbers:"+d);
+   System.out.println("The product of two numbers:"+e);
+   System.out.println("The division of two numbers:"+f);
+  }
+}
